07d30643ba77aa72cad1c6a1233e4cd18ac45c34
dhrubobarman <dhrubobarman847@gmail.com>
2022-07-21 16:06:59 +0530

    Final result

--- src/component/Ground.jsx
+++ src/component/Ground.jsx
@@ -2,19 +2,12 @@ import React, { useEffect } from "react";
 import { useFrame, useLoader } from "@react-three/fiber";
 import { MeshReflectorMaterial } from "@react-three/drei";
 import { LinearEncoding, RepeatWrapping, TextureLoader } from "three";
-import terrainNormal from "..//textures/terrain-normal.jpg";
-import terrainRoughness from "..//textures/terrain-roughness.jpg";
 
 export function Ground() {
   // thanks to https://polyhaven.com/a/rough_plasterbrick_05 !
-  // const [roughness, normal] = useLoader(TextureLoader, [
-  //   process.env.PUBLIC_URL + "textures/terrain-roughness.jpg",
-  //   process.env.PUBLIC_URL + "textures/terrain-normal.jpg",
-  // ]);
-
   const [roughness, normal] = useLoader(TextureLoader, [
-    terrainNormal,
-    terrainRoughness,
+    process.env.PUBLIC_URL + "textures/terrain-roughness.jpg",
+    process.env.PUBLIC_URL + "textures/terrain-normal.jpg",
   ]);
 
   useEffect(() => {
